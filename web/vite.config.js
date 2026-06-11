422fad9f7d4f0c22a9ffae896fae4e662af6aa34
norekfx <noe2201@o2.pl>
2026-06-11 13:59:43 +0200

    Allow production host in Vite dev server

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ["player.mikswisite.cc"],
     proxy: {
       "/api": {
         target: "http://api:8090",
